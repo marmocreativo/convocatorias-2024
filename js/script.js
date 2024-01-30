a9a416f1a9fe1247356f156f7fae83c174a8ceb9
gonzalo torres <conchax@gmail.com>
2024-01-30 15:37:38 -0600

    collapse media kit

--- js/script.js
+++ js/script.js
@@ -15,7 +15,30 @@ jQuery( ".arrow-icon" ).click(function() {
 
 
 if (window.matchMedia("(max-width: 575.98px)").matches) {
- 
-} else {
-  /* La pantalla tiene menos de 400 píxeles de ancho */
+  document.getElementById("compartir").setAttribute("data-bs-toggle", "collapse");
+  document.getElementById("compartir").setAttribute("data-bs-target","#collapseCompartir");
+  document.getElementById("descargar").setAttribute("data-bs-toggle", "collapse");
+  document.getElementById("descargar").setAttribute("data-bs-target","#collapseDescargar");
+  document.getElementById("collapseCompartir").setAttribute("class","collapse")
+  document.getElementById("collapseDescargar").setAttribute("class","collapse")
+
+} else { 
+  document.getElementById("compartir").removeAttribute("data-bs-toggle");
+  document.getElementById("compartir").removeAttribute("data-bs-target");
+  document.getElementById("collapseCompartir").removeAttribute("class");
+
+  document.getElementById("descargar").removeAttribute("data-bs-toggle");
+  document.getElementById("descargar").removeAttribute("data-bs-target");
+  document.getElementById("collapseDescargar").removeAttribute("class");
 }
+
+
+jQuery(function(){  
+
+  jQuery(document).ready(function(){ 
+  jQuery('.collapse').on('show.bs.collapse',function(){
+  jQuery('.collapse.show').collapse('toggle');
+  });
+});
+
+});
